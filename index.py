0c10cba9069ba3b68037853de6fcce107d307de1
Andrew Bounds <andybounds@gmail.com>
2020-10-13 10:50:01 -0400

    Code Updates

--- index.py
+++ index.py
@@ -3,12 +3,9 @@ import dash_bootstrap_components as dbc
 
 from homepage import Homepage
 
-def run():
-    app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
-    app.layout = Homepage()
-
-    app.run_server(host='0.0.0.0')
+app.layout = Homepage()
 
 if __name__ == "__main__":
-    run()
+    app.run_server(host='0.0.0.0')
